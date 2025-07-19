79144cf1e25f9f9ec29696c97949430c18c62d3e
dev <49467408+DevvyDont@users.noreply.github.com>
2025-07-18 22:06:27 -0400

    backpack recipes

--- src/main/java/xyz/devvydont/smprpg/items/blueprints/storage/ColossalBackpack.java
+++ src/main/java/xyz/devvydont/smprpg/items/blueprints/storage/ColossalBackpack.java
@@ -3,13 +3,23 @@ package xyz.devvydont.smprpg.items.blueprints.storage;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.CraftingRecipe;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.recipe.CraftingBookCategory;
+import xyz.devvydont.smprpg.SMPRPG;
 import xyz.devvydont.smprpg.items.CustomItemType;
 import xyz.devvydont.smprpg.items.base.BackpackBase;
+import xyz.devvydont.smprpg.items.interfaces.ICraftable;
 import xyz.devvydont.smprpg.items.interfaces.IModelOverridden;
 import xyz.devvydont.smprpg.services.ItemService;
 import xyz.devvydont.smprpg.util.formatting.ComponentUtils;
 
-public class ColossalBackpack extends BackpackBase implements IModelOverridden {
+import java.util.Collection;
+import java.util.List;
+
+public class ColossalBackpack extends BackpackBase implements IModelOverridden, ICraftable {
 
     public ColossalBackpack(ItemService itemService, CustomItemType type) {
         super(itemService, type);
@@ -33,5 +43,31 @@ public class ColossalBackpack extends BackpackBase implements IModelOverridden {
     @Override
     public Material getDisplayMaterial() {
         return Material.ORANGE_BUNDLE;
+    }
+
+    @Override
+    public NamespacedKey getRecipeKey() {
+        return new NamespacedKey(SMPRPG.getInstance(), this.getCustomItemType().getKey() + "_recipe");
+    }
+
+    @Override
+    public CraftingRecipe getCustomRecipe() {
+        var recipe = new ShapedRecipe(this.getRecipeKey(), generate());
+        recipe.setCategory(CraftingBookCategory.EQUIPMENT);
+        recipe.shape("mmm", "mbm", "mmm");
+        recipe.setIngredient('m', ItemService.generate(CustomItemType.ENCHANTED_SHULKER_SHELL));
+        recipe.setIngredient('b', ItemService.generate(CustomItemType.GIGANTIC_BACKPACK));
+        return recipe;
+    }
+
+    /**
+     * A collection of items that will unlock the recipe for this item. Typically will be one of the components
+     * of the recipe itself, but can be set to whatever is desired
+     *
+     * @return
+     */
+    @Override
+    public Collection<ItemStack> unlockedBy() {
+        return List.of(ItemService.generate(CustomItemType.ENCHANTED_SHULKER_SHELL));
     }
 }
--- src/main/java/xyz/devvydont/smprpg/items/blueprints/storage/SmallBackpack.java
+++ src/main/java/xyz/devvydont/smprpg/items/blueprints/storage/SmallBackpack.java
@@ -3,13 +3,23 @@ package xyz.devvydont.smprpg.items.blueprints.storage;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.CraftingRecipe;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.recipe.CraftingBookCategory;
+import xyz.devvydont.smprpg.SMPRPG;
 import xyz.devvydont.smprpg.items.CustomItemType;
 import xyz.devvydont.smprpg.items.base.BackpackBase;
+import xyz.devvydont.smprpg.items.interfaces.ICraftable;
 import xyz.devvydont.smprpg.items.interfaces.IModelOverridden;
 import xyz.devvydont.smprpg.services.ItemService;
 import xyz.devvydont.smprpg.util.formatting.ComponentUtils;
 
-public class SmallBackpack extends BackpackBase implements IModelOverridden {
+import java.util.Collection;
+import java.util.List;
+
+public class SmallBackpack extends BackpackBase implements IModelOverridden, ICraftable {
 
     public SmallBackpack(ItemService itemService, CustomItemType type) {
         super(itemService, type);
@@ -33,5 +43,31 @@ public class SmallBackpack extends BackpackBase implements IModelOverridden {
     @Override
     public Component getInterfaceTitleComponent() {
         return ComponentUtils.create("Small Backpack", NamedTextColor.BLACK);
+    }
+
+    @Override
+    public NamespacedKey getRecipeKey() {
+        return new NamespacedKey(SMPRPG.getInstance(), this.getCustomItemType().getKey() + "_recipe");
+    }
+
+    @Override
+    public CraftingRecipe getCustomRecipe() {
+        var recipe = new ShapedRecipe(this.getRecipeKey(), generate());
+        recipe.setCategory(CraftingBookCategory.EQUIPMENT);
+        recipe.shape("mmm", "mbm", "mmm");
+        recipe.setIngredient('m', ItemService.generate(Material.LEATHER));
+        recipe.setIngredient('b', ItemService.generate(Material.CHEST));
+        return recipe;
+    }
+
+    /**
+     * A collection of items that will unlock the recipe for this item. Typically will be one of the components
+     * of the recipe itself, but can be set to whatever is desired
+     *
+     * @return
+     */
+    @Override
+    public Collection<ItemStack> unlockedBy() {
+        return List.of(ItemService.generate(Material.LEATHER));
     }
 }
