fcc8c352abd6448302414b24443c962185045a52
dev <49467408+DevvyDont@users.noreply.github.com>
2025-07-28 23:13:21 -0400

    fishing: rarity no longer affects fishing exp

--- src/main/java/xyz/devvydont/smprpg/skills/listeners/FishingExperienceListener.java
+++ src/main/java/xyz/devvydont/smprpg/skills/listeners/FishingExperienceListener.java
@@ -25,16 +25,6 @@ public class FishingExperienceListener implements Listener {
     }
 
     /**
-     * Get the multiplier of skill experience to apply to a skill experience reward for fishing up a fish.
-     * @param rarity The rarity of the fish.
-     * @return An experience multiplier.
-     */
-    private int getFishRarityMultiplier(ItemRarity rarity) {
-        var x = rarity.ordinal() + 1;
-        return x * x;
-    }
-
-    /**
      * Simply listen for when a fish loot event happens. Fishing EXP is on the reward itself.
      * We should also give a bonus for fishing higher quality fish.
      * @param event The {@link FishingLootGenerateEvent} event that provides us with relevant context.
@@ -45,16 +35,9 @@ public class FishingExperienceListener implements Listener {
         // All we need to do is give the player experience.
         var exp = event.getCalculationResult().Reward().Element().getFishingExperience();
 
-        // As a bonus, let's give the player extra experience if they fished up a rare fish.
-        var isFish = event.getCalculationResult().Type().Element().equals(FishingLootType.FISH);
-        if (isFish && event.getCaughtEntity() instanceof Item item)
-            exp *= getFishRarityMultiplier(ItemService.blueprint(item.getItemStack()).getRarity(item.getItemStack()));
-
         // Award.
         var player = SMPRPG.getService(EntityService.class).getPlayerInstance(event.getFishingContext().getPlayer());
         player.getFishingSkill().addExperience(exp, SkillExperienceGainEvent.ExperienceSource.FISH);
-
     }
-
 
 }
