be322b85263d24aff046950ee7ae743831d500d7
dev <49467408+DevvyDont@users.noreply.github.com>
2025-07-28 23:30:42 -0400

    fishing: unique fish 2x exp

--- src/main/java/xyz/devvydont/smprpg/skills/listeners/FishingExperienceListener.java
+++ src/main/java/xyz/devvydont/smprpg/skills/listeners/FishingExperienceListener.java
@@ -1,5 +1,8 @@
 package xyz.devvydont.smprpg.skills.listeners;
 
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.Sound;
 import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -8,9 +11,13 @@ import xyz.devvydont.smprpg.SMPRPG;
 import xyz.devvydont.smprpg.events.skills.SkillExperienceGainEvent;
 import xyz.devvydont.smprpg.fishing.events.FishingLootGenerateEvent;
 import xyz.devvydont.smprpg.fishing.loot.FishingLootType;
+import xyz.devvydont.smprpg.fishing.utils.FishingGallery;
 import xyz.devvydont.smprpg.items.ItemRarity;
 import xyz.devvydont.smprpg.services.EntityService;
 import xyz.devvydont.smprpg.services.ItemService;
+import xyz.devvydont.smprpg.util.formatting.ComponentUtils;
+import xyz.devvydont.smprpg.util.persistence.KeyStore;
+import xyz.devvydont.smprpg.util.persistence.PDCAdapters;
 
 /**
  * Responsible for awarding fishing experience for general fishing events.
@@ -34,6 +41,20 @@ public class FishingExperienceListener implements Listener {
 
         // All we need to do is give the player experience.
         var exp = event.getCalculationResult().Reward().Element().getFishingExperience();
+
+        // Double it if it's the first one.
+        var gallery = event.getFishingContext().getPlayer().getPersistentDataContainer().getOrDefault(KeyStore.FISHING_GALLERY, PDCAdapters.FISHING_GALLERY, new FishingGallery());
+        var element = gallery.get(event.getCalculationResult().Reward().Element());
+        if (element == 0) {
+            event.getFishingContext().getPlayer().sendMessage(ComponentUtils.alert(ComponentUtils.merge(
+                    ComponentUtils.create("UNIQUE CATCH!", NamedTextColor.GREEN).decoration(TextDecoration.BOLD, true),
+                    ComponentUtils.create(" That's a new one! This fish gave you "),
+                    ComponentUtils.create("double experience", NamedTextColor.AQUA),
+                    ComponentUtils.create("!")
+            )));
+            event.getFishingContext().getPlayer().playSound(event.getFishingContext().getPlayer().getLocation(), Sound.ENTITY_VILLAGER_YES, 1, 1.25f);
+            exp *= 2;
+        }
 
         // Award.
         var player = SMPRPG.getService(EntityService.class).getPlayerInstance(event.getFishingContext().getPlayer());
