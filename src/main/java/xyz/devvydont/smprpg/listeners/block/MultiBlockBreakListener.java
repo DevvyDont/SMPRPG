290004a826ab67b4da726087d5ff72dfc8b9010e
CheezedFish <watdou606@gmail.com>
2025-08-07 17:32:39 -0400

    Multibreak/Woodcutting: Add fuzzy block support, 6-side wood blocks now give woodcutting xp

--- src/main/java/xyz/devvydont/smprpg/listeners/block/MultiBlockBreakListener.java
+++ src/main/java/xyz/devvydont/smprpg/listeners/block/MultiBlockBreakListener.java
@@ -6,8 +6,8 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
-import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.server.ServerLoadEvent;
 import xyz.devvydont.smprpg.SMPRPG;
 import xyz.devvydont.smprpg.attribute.AttributeWrapper;
 import xyz.devvydont.smprpg.events.skills.SkillExperienceGainEvent;
@@ -16,27 +16,50 @@ import xyz.devvydont.smprpg.services.EntityService;
 import xyz.devvydont.smprpg.skills.SkillInstance;
 import xyz.devvydont.smprpg.skills.listeners.ForagingExperienceListener;
 import xyz.devvydont.smprpg.util.listeners.ToggleableListener;
-import xyz.devvydont.smprpg.util.time.TickTime;
 import xyz.devvydont.smprpg.util.world.ChunkUtil;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class MultiBlockBreakListener extends ToggleableListener {
 
     public static final Set<Material> MULTI_BREAK_LOGS = Set.of(
             Material.OAK_LOG,
+            Material.OAK_WOOD,
             Material.BIRCH_LOG,
+            Material.BIRCH_WOOD,
             Material.SPRUCE_LOG,
+            Material.SPRUCE_WOOD,
             Material.JUNGLE_LOG,
+            Material.JUNGLE_WOOD,
             Material.DARK_OAK_LOG,
+            Material.DARK_OAK_WOOD,
             Material.ACACIA_LOG,
+            Material.ACACIA_WOOD,
             Material.MANGROVE_LOG,
+            Material.MANGROVE_WOOD,
             Material.CHERRY_LOG,
+            Material.CHERRY_WOOD,
             Material.PALE_OAK_LOG,
+            Material.PALE_OAK_WOOD,
             Material.CRIMSON_STEM,
-            Material.WARPED_STEM
+            Material.CRIMSON_HYPHAE,
+            Material.WARPED_STEM,
+            Material.WARPED_HYPHAE
     );
+
+    public static final HashMap<Material, Set<Material>> FUZZY_BLOCKS = new HashMap<Material, Set<Material>>();
+    public static final Set<Material> DEFAULT_FUZZY_BLOCK_SET = Set.of();
+
+    public static void __initFuzzyBlocks() {
+        FUZZY_BLOCKS.put(Material.OAK_LOG,          Set.of(Material.OAK_LOG, Material.OAK_WOOD));
+        FUZZY_BLOCKS.put(Material.OAK_WOOD,         Set.of(Material.OAK_LOG, Material.OAK_WOOD));
+        FUZZY_BLOCKS.put(Material.BIRCH_LOG,        Set.of(Material.BIRCH_LOG, Material.BIRCH_WOOD));
+        FUZZY_BLOCKS.put(Material.BIRCH_WOOD,       Set.of(Material.BIRCH_LOG, Material.BIRCH_WOOD));
+        FUZZY_BLOCKS.put(Material.SPRUCE_LOG,       Set.of(Material.SPRUCE_LOG, Material.SPRUCE_WOOD));
+        FUZZY_BLOCKS.put(Material.SPRUCE_WOOD,      Set.of(Material.SPRUCE_LOG, Material.SPRUCE_WOOD));
+        FUZZY_BLOCKS.put(Material.JUNGLE_LOG,       Set.of(Material.JUNGLE_LOG, Material.JUNGLE_WOOD));
+        FUZZY_BLOCKS.put(Material.JUNGLE_WOOD,      Set.of(Material.JUNGLE_LOG, Material.JUNGLE_WOOD));
+    }
 
     private void stepAdjacentBlocks(Block startBlock, int blocksLeft, Set<Block> checkedBlocks, Player player, Set<Block> blocksToBreak) {
         if (blocksLeft <= 0) {
@@ -61,8 +84,11 @@ public class MultiBlockBreakListener extends ToggleableListener {
                     if (ChunkUtil.isBlockSkillInvalid(adjBlock))
                         continue;
 
-                    // Check for material equivalency
-                    if (adjBlock.getType() == startBlock.getType()) {
+                    // Check for material validity
+                    // Check for equality first, then go through fuzzy blocks to improve performance.
+                    var startType = startBlock.getType();
+                    var fuzzies = FUZZY_BLOCKS.getOrDefault(startType, DEFAULT_FUZZY_BLOCK_SET);
+                    if (adjBlock.getType() == startBlock.getType() || fuzzies.contains(adjBlock.getType())) {
                         // Success! Reduce our blocks left to chop and delay our chop for a few ticks.
                         blocksLeft--;
                         blocksToBreak.add(adjBlock);
@@ -100,6 +126,11 @@ public class MultiBlockBreakListener extends ToggleableListener {
             }, delay);
             delay += 1;
         }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void registerFuzzyMaterials(ServerLoadEvent event) {
+        __initFuzzyBlocks();
     }
 
 }
